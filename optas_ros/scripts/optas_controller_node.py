7258de08c516ea5e70ee0be5548b47771ca958b9
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-11-24 14:01:24 +0000

    Add shebangs

--- optas_ros/scripts/optas_controller_node.py
+++ optas_ros/scripts/optas_controller_node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import JointState
 from optas_ros.node import Node
--- optas_ros/scripts/optas_planner_node.py
+++ optas_ros/scripts/optas_planner_node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rospy
 from optas_ros.node import Node
 
