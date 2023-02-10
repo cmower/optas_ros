adc36a9ca2627262fde9ab557af55104d315cfb7
mhubii <m.huber_1994@hotmail.de>
2023-02-10 13:11:09 +0000

    added robot state publisher, formatted

--- optas_ros/launch/linear_differential_ik_controller_example.launch.py
+++ optas_ros/launch/linear_differential_ik_controller_example.launch.py
@@ -1,16 +1,18 @@
 import os
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable
+
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
 
     # Load robot description
     xacro_filename = os.path.join(
         get_package_share_directory("lbr_storz_tilt_endoscope_description"),
-        'urdf',
-        'lbr_storz_tilt_endoscope.urdf.xacro',
+        "urdf",
+        "lbr_storz_tilt_endoscope.urdf.xacro",
     )
 
     robot_description = Command([FindExecutable(name="xacro"), " ", xacro_filename])
@@ -28,26 +30,37 @@ def generate_launch_description():
 
     # Setup controller node
     controller_node = Node(
-        package='optas_ros',
-        executable='optas_controller_node',
-        name='controller',
+        package="optas_ros",
+        executable="optas_controller_node",
+        name="controller",
         parameters=[
             {"robot_description": robot_description},
             {"script": script_filename},
             {"config": config_filename},
             {"class_name": "LinearDifferentialIKController"},
             {"sampling_frequency": 100},
-        ]
+        ],
+    )
+
+    # Robot state publisher
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[robot_description],
     )
 
     # Setup rviz
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
     )
 
-    return LaunchDescription([
-        controller_node,
-        rviz_node,
-    ])
+    return LaunchDescription(
+        [
+            controller_node,
+            robot_state_publisher_node,
+            rviz_node,
+        ]
+    )
